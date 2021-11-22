cea1f9f63d7f2743f3291dabef6b598c625acbef
dimaaasex911 <dimaaasex911@gmail.com>
2021-11-22 17:54:39 +0200

    fail

--- python/main.py
+++ python/main.py
@@ -1,4 +1,6 @@
 from flask import Flask
+from minio import Minio
+from minio.error import S3Error
 
 app = Flask(__name__)
 
@@ -6,5 +8,20 @@ app = Flask(__name__)
 def hello_world():
     return "<h1>Hello, World!<h1>"
 
+#---minio_connect---#
+client = Minio('172.19.0.2:9000',
+               access_key='amdmap91',
+               secret_key='165krasavchikov',
+               secure=False)
+
+#---minio_return_buckets---#
+buckets = client.list_buckets()
+def return_buckets():
+    buckets_temp = []
+    for bucket in buckets:
+        buckets_temp.append(bucket)
+        print(bucket.name, bucket.creation_date)
+    return buckets_temp
+        
 if __name__ == '__main__':
    app.run(host='0.0.0.0')
