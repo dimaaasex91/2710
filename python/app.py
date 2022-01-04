82c9784c5d32d3cbbd91b178170173acc60acd03
dimaaasex911 <dimaaasex911@gmail.com>
2022-01-04 14:26:10 +0200

    install pip

--- python/app.py
+++ python/app.py
@@ -1,10 +1,5 @@
 from flask import Flask, render_template, flash, request, redirect
-#from werkzeug.utils import secure_filename
-
-# Copyright © 2021 Markus W Mahlberg
-# This work is free. You can redistribute it and/or modify it under the
-# terms of the Do What The Fuck You Want To Public License, Version 2,
-# as published by Sam Hocevar. See the LICENSE file for more details.
+from werkzeug.utils import secure_filename
 from minio import Minio
 import os,io
 
