28a4c11801e5890f1eb0c5862a76aee3b2c828b0
dimaaasex911 <dimaaasex911@gmail.com>
2022-01-04 15:22:35 +0200

    change app

--- python/app.py
+++ python/app.py
@@ -20,40 +20,39 @@ def allowed_file(filename):
 
 @app.route("/")
 def index():
-  if not client.bucket_exists("images"):
-    print("Foo")
-    return render_template('error.html',
+    if not client.bucket_exists("images"):
+      print("Foo")
+      return render_template('error.html',
       status_text="Internal Server Error",
       msg="Bucket \"images\" does not exist!",
       code=500,
       ), 500
-  images = client.list_objects("images")
-  return render_template('index.html',prefix=app.config['PREFIX'],images=images)
+    images = client.list_objects("images")
+    return render_template('index.html',prefix=app.config['PREFIX'],images=images)
 
 @app.route("/upload",methods=['POST'])
 def upload_file():
+    if 'file' not in request.files:
+      flash('No file!')
+      return redirect(request.url)
 
-  if 'file' not in request.files:
-    flash('No file!')
-    return redirect(request.url)
+    file = request.files['file']
 
-  file = request.files['file']
+    if file.filename == '':
+        flash('No selected file')
+        return redirect("/")
 
-  if file.filename == '':
-    flash('No selected file')
+    if file and allowed_file(file.filename):
+        filename = secure_filename(file.filename)
+        result = client.put_object(
+          bucket_name="images",
+          object_name=filename,
+          data=file.stream,
+          length=-1,
+          part_size=10*1024*1024,
+          content_type=file.content_type)
+        print("Uploaded %s with etag %s to minio",filename,result.etag)
+        return redirect("/")
     return redirect("/")
-
-  if file and allowed_file(file.filename):
-      filename = secure_filename(file.filename)
-      result = client.put_object(
-        bucket_name="images",
-        object_name=filename,
-        data=file.stream,
-        length=-1,
-        part_size=10*1024*1024,
-        content_type=file.content_type)
-      print("Uploaded %s with etag %s to minio",filename,result.etag)
-      return redirect("/")
-  return redirect("/")
 if __name__ == "__main__":
     app.run(host=os.getenv("APP_HOST","0.0.0.0"),port=os.getenv("APP_PORT","5000"))   #add "" to 5000
