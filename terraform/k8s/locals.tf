a536bc0740b6da6bf70882f76900def48bb00d9f
dimaaasex911 <dimaaasex911@gmail.com>
2022-01-25 11:58:36 +0200

    argocd chart

--- terraform/k8s/locals.tf
+++ terraform/k8s/locals.tf
@@ -2,13 +2,14 @@ locals {
   k8s = {
       k8s_host              = "https://master1.myownserv.space:6443"
   }
-}
 
-helm = {
-  helm_gitlab_version                 = "0.23.0"
-  helm_prometheus_operator_version    = "12.3.0"
-  helm_argocd_version                 = "3.31.0"
-  gitlab_helm_repository              = "https://charts.gitlab.io"
-  prometheus_helm_repository          = "https://prometheus-community.github.io/helm-charts"
-  argocd_helm_repository              = "https://argoproj.github.io/argo-helm"
+
+  helm = {
+    helm_gitlab_version                 = "0.23.0"
+    helm_prometheus_operator_version    = "12.3.0"
+    helm_argocd_version                 = "3.31.0"
+    gitlab_helm_repository              = "https://charts.gitlab.io"
+    prometheus_helm_repository          = "https://prometheus-community.github.io/helm-charts"
+    argocd_helm_repository              = "https://argoproj.github.io/argo-helm"
+  }
 }
